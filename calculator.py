2a33ddb6f2e1293513047215776d1c3e2cf99fdc
lotsofluck <anitabangali@yahoo.com>
2020-09-03 19:53:14 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,14 +1,19 @@
-#Hello and welcome to the fluffy hat emporium!
+#Hello and welcome you for coming to the Fluffy Hat Emporium!
 #This is a calculator that uses the circumference of your head and the length of your forehead to calculate
-# the perfect amount of fabric needed for your custom hat.
+# the fluff fusion equation for your custom hat.
 name=input("Enter your name:")
 print("Hello "+ name+"!")
-circumference=input("please enter the circumference of your head")
-#measure the circumference with a measuring tape, don't forget to slip one finger in the side for extra space!
+circumference=input("Please enter the circumference of your head")#in inches
+#Measure the circumference with a measuring tape, don't forget to slip one finger in the side for extra space!
 circumference=int(circumference)
 print(circumference)
-hat_length=input("please enter the length of your forehead minus 2")
+hat_length=input("Please enter the length of your forehead minus 2")#in inches
+#The subtracting 2 is so your hat will rest perfectly on your forehead to showcase your face :)
 hat_length=int(hat_length)
 print(hat_length)
-in_fabric=(circumference * hat_length)
-print(in_fabric)
+in_fabric=(circumference * hat_length) #in in the variable name stands for inches
+in_fabric=int(in_fabric)
+fave_color=input("What is your favorite color?")
+fluff_preference=input("Would you like a small or large puffball?")
+fluffy="Your fluff fusion equation is %s of %s warm fabric with a %s puffball!" % (in_fabric,fave_color,fluff_preference)
+print(fluffy)
